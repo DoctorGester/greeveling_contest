5c6f48d14f468c08958d33500720663796d1e88f
doctorgester <doctorgester@gmail.com>
2017-11-23 20:34:31 +0300

    Added a bunch of modifier tooltips

--- game/scripts/vscripts/modifiers/modifier_egg_hatch_pause_target.lua
+++ game/scripts/vscripts/modifiers/modifier_egg_hatch_pause_target.lua
@@ -1,6 +1,10 @@
 ---@class modifier_egg_hatch_pause_target : CDOTA_Modifier_Lua
 modifier_egg_hatch_pause_target = {}
 
+function modifier_egg_hatch_pause_target:IsHidden()
+    return true
+end
+
 function modifier_egg_hatch_pause_target:CheckState()
     return {
         [MODIFIER_STATE_INVULNERABLE] = true,
