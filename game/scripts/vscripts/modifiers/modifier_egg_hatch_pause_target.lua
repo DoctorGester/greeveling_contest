18feba34dfa6c882cdb9b4845c77fc4f4fd7fb97
doctorgester <doctorgester@gmail.com>
2017-11-23 20:06:00 +0300

    Removed courier from the shop

--- game/scripts/vscripts/modifiers/modifier_egg_hatch_pause_target.lua
+++ game/scripts/vscripts/modifiers/modifier_egg_hatch_pause_target.lua
@@ -1,14 +1,6 @@
 ---@class modifier_egg_hatch_pause_target : CDOTA_Modifier_Lua
 modifier_egg_hatch_pause_target = {}
 
-function modifier_egg_hatch_pause_target:OnCreated(params)
-    print("He's, in", IsServer())
-end
-
-function modifier_egg_hatch_pause_target:OnDestroy()
-    print("He's, out", IsServer())
-end
-
 function modifier_egg_hatch_pause_target:CheckState()
     return {
         [MODIFIER_STATE_INVULNERABLE] = true,
