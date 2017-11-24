cb0c6418b2cb4519fc258df7b58510e522dcefe9
doctorgester <doctorgester@gmail.com>
2017-11-24 03:25:23 +0300

    Fixed static discharge being applied to towers

--- game/scripts/vscripts/bonuses/greater/modifier_static_discharge.lua
+++ game/scripts/vscripts/bonuses/greater/modifier_static_discharge.lua
@@ -12,6 +12,10 @@ function modifier_static_discharge:DeclareFunctions()
 end
 
 function modifier_static_discharge:OnTakeDamage(damage_data)
+    if damage_data.unit:IsBuilding() or damage_data.unit:IsTower() then
+        return false
+    end
+
     if damage_data.attacker == self:GetParent() and not damage_data.unit:HasModifier("modifier_static_discharge_cooldown") then
         damage_data.unit:AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_static_discharge_target", { duration = 4.0 })
     end
